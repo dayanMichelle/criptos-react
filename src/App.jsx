26a3c0e0ccb521fe12e3c51eb1e0c3af91df797c
dayanMichelle <dayaara6@gmail.com>
2022-04-12 14:21:52 -0400

    header

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled"
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Heading = styled.h1`
+  font-family: 'Anek Tamil', sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto
+  }
+`
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width:90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width:80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+
+function App() {
+
+  return (
+    <Container>
+      <Imagen src={ImagenCripto} alt='Imagen' />
+      <div>
+        <Heading>Trade Cryptocurrencies Instantly</Heading>
+      </div>
+
+    </Container>
+  )
+}
+
+export default App
