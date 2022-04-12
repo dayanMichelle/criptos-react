a59caca6c87c1f5647f17fa347ce9e96d9b13767
dayanMichelle <dayaara6@gmail.com>
2022-04-12 15:22:48 -0400

    select styls

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled"
 import { useSelectCoins } from "../hooks/useSelectCoins";
+import {coins} from "../data/coins";
 
 const InputSubmit = styled.input`
   background-color: #9497FF;
@@ -12,6 +13,7 @@ const InputSubmit = styled.input`
   font-size: 20px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
+  margin-top: 30px;
   &:hover {
     background-color: #7A7DFE;
     cursor: pointer;
@@ -19,7 +21,7 @@ const InputSubmit = styled.input`
 `
 export const Form = () => {
 
-  const [SelectCoins] = useSelectCoins('Chose your coin');
+  const [SelectCoins] = useSelectCoins('Chose your coin',coins);
 
   SelectCoins()
   return (
---
+++ src/data/coins.js
@@ -0,0 +1,6 @@
+export  const coins = [
+  { id: 'USD', name: 'Dolar de Estados Unidos' },
+  { id: 'XMN', name: 'Peso mexicano' },
+  { id: 'EUR', name: 'Euro' },
+  { id: 'GBP', name: 'Libra esterlina' },
+]
